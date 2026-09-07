8d3cfc176283053b826b57e5d884a08a76bd170f
malte-b <malte.barth01@gmail.com>
2026-09-07 15:43:51 +0200

    fix: remainling failing tests

--- services/wallet-frontend/playwright.remote.config.ts
+++ services/wallet-frontend/playwright.remote.config.ts
@@ -4,15 +4,16 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
 	testDir: "./tests",
 	timeout: 60000,
+	retries: typeof process !== "undefined" && process.env.CI ? 2 : 0,
 	workers: typeof process !== "undefined" && process.env.CI ? 2 : undefined,
 	fullyParallel: false,
 	use: {
 		baseURL:
 			process.env.PLAYWRIGHT_BASE_URL ||
 			"https://staging.bf.citylab-berlin.org",
-		trace: "on",
-		screenshot: "on",
-		video: "on",
+		trace: "on-first-retry",
+		screenshot: "only-on-failure",
+		video: "on-first-retry",
 		viewport: { width: 390, height: 844 }, // Mobile viewport since Klaro is mobile-first
 		deviceScaleFactor: 3,
 		isMobile: true,
