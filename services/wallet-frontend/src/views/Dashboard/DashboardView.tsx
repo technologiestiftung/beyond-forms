96844b7a21afbb1e67233839d051e0538579d87d
malte-b <malte.barth01@gmail.com>
2026-09-10 12:03:41 +0200

    feat: add dummy application cards for new forms

--- services/wallet-frontend/src/views/Dashboard/DashboardView.tsx
+++ services/wallet-frontend/src/views/Dashboard/DashboardView.tsx
@@ -155,10 +155,36 @@ export const DashboardView: React.FC = () => {
 					formType="antrag_wohngeld"
 				/>
 
+				<SimpleApplicationCard
+					title={t(
+						"sections.applications.basic_income.title",
+						"Grundsicherungsgeld",
+					)}
+					description={t(
+						"sections.applications.basic_income.description",
+						"Beantrage Grundsicherungsgeld direkt mit Deinen hinterlegten Angaben.",
+					)}
+					formType="antrag_grundsicherungsgeld"
+				/>
+
+				<SimpleApplicationCard
+					title={t(
+						"sections.applications.child_allowance.title",
+						"Kinderzuschlag",
+					)}
+					description={t(
+						"sections.applications.child_allowance.description",
+						"Beantrage Kinderzuschlag direkt mit Deinen hinterlegten Angaben.",
+					)}
+					formType="antrag_kinderzuschlag"
+				/>
+
+				{/*
+				Commented out for now as we don't want to use tutorials yet
 				<DashboardTutorials
 					tutorials={tutorials}
 					activeLanguage={activeLanguage}
-				/>
+				/>*/}
 
 				{/*
 				Commented out for now as we don't have an emergency info panel yet
